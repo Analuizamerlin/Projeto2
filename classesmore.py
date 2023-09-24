04e244ce21a9fa0916fb5077265675bd24e1e33d
Ana Luiza <analuizamerlin01@gmail.com>
2023-09-24 14:18:28 -0300

    Tratamento erros usando regex

--- classesmore.py
+++ classesmore.py
@@ -1,10 +1,13 @@
+import re
+
 class Pessoa:
-    def __init__(self,nome, idade, altura, peso):
+    def __init__(self,nome, idade, altura, peso, nascimento):
         self.nome = nome
         self.idade = idade
         self.altura = altura
         self.peso = peso
-        
+        self.nascimento = nascimento
+
     def eh_maior(self):
         if int(self.idade) >= 18:
             return f'{self.nome} é maior de idade!'
@@ -32,16 +35,46 @@ class Pessoa:
     def apresentar(self):
         print(f'{self.nome} tem {self.idade} anos, {self.altura}m de altura e pesa {self.peso}kg!\n')
 
-lista = []
-lista.append(Pessoa(input('Nome: '),input('Idade: '),input('Altura: '),input('Peso: ')))
-mais = input('\nDeseja adicionar mais alguém? y/n \n')
+nome = input('Nome: ')
+while True:
+    if not re.match(r'^[a-zA-Z\s]+$', nome):
+        print('Entrada inválida. Tente novamente!')
+        nome = input('Nome: ')
+    else: 
+        break
+    
+idade = input('Idade: ')
+while True:
+    if not re.match(r'^[0-9]+$', idade):
+        print('Entrada inválida. Tente novamente!')
+        idade = input('Idade: ')
+    else: 
+        break
+altura = input('Altura: ')
+while True:
+    if not re.match(r'^[1-9]\.[0-9]+$|^0\.[1-9]+$', altura):
+        print('Entrada inválida. Tente novamente!')
+        altura = input('Altura: ')
+    else: 
+        break
+peso = input('Peso: ')
+while True:
+    if not re.match(r'^[1-9][0-9]*[0-9]*\.[0-9]', peso)or float(peso)<=0:
+        print('Entrada inválida. Tente novamente!')
+        peso = input('Peso: ')
+    else: 
+        break
+nascimento = input('Data de Nascimento(dd/mm/aaaa): ')
+while True:
+    if not re.match(r'^(0[0-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}+$', nascimento):
+        print('Entrada inválida. Tente novamente!')
+        nascimento = input('Data de Nascimento(dd/mm/aaaa): ')
+    else: 
+        break
 
-while mais == 'y':
-    lista.append(Pessoa(input('\nNome: '),input('Idade: '),input('Altura: '),input('Peso: ')))
-    mais = input('\nDeseja adicionar mais alguém? y/n \n')
+pessoa1 = Pessoa(nome, idade, altura, peso, nascimento)
 
-for i in lista:
-    print(f'\n{i.eh_maior()}')
-    print(i.imc())
-    print(i.imc_longo())
-    i.apresentar()
+print(f'\n{pessoa1.eh_maior()}')
+print(pessoa1.imc())
+print(pessoa1.imc_longo())
+pessoa1.apresentar()
